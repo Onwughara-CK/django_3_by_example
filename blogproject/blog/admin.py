8b72323299f0d82d9e55c7386fc8eaaefdc5ce63
Onwughara-CK <kelechicollins.93@gmail.com>
2020-08-26 19:17:47 +0100

    customize the admin site for post fieldsets

--- blogproject/blog/admin.py
+++ blogproject/blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+# admin.site.register(Post)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'publish', 'status']
+    list_filter = ['created', 'author', 'publish', 'status']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+    raw_id_fields = ['author']
+
+
+admin.site.register(Post, PostAdmin)
